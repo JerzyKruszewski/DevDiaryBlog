64b02346655b85b5d4816c31a2bd1852c15fbc14
Jerzy Kruszewski <jerzyj.kruszewski@gmail.com>
2021-02-11 23:15:32 +0100

    Added Users API with register handler

---
+++ DevDiaryBlog.Web/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using DevDiaryBlog.Web.Model;
+using DevDiaryBlog.Web.Services.Interfaces;
+using DevDiaryBlog.Web.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevDiaryBlog.Web.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<User> RegisterUser(UserViewModel model)
+        {
+            return await _service.RegisterUser(model.Login, model.Password, model.Username);
+        }
+    }
+}
---
+++ DevDiaryBlog.Web/ViewModels/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevDiaryBlog.Web.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Login { init; get; }
+
+        public string Password { get; set; }
+
+        public string Username { get; set; }
+    }
+}
